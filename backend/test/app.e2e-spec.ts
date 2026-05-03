4e573314ee3892f1675432ec576ed0d66e083ac1
Alexandr <andreevqt@users.noreply.github.com>
2026-05-03 16:45:10 +0300

    fix: fix e2e test imports and add check:ts script to CI

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
-import * as request from 'supertest';
-import * as cookieParser from 'cookie-parser';
+import request = require('supertest');
+import cookieParser = require('cookie-parser');
 import { AppModule } from '../src/app.module';
 import { HttpExceptionFilter } from '../src/common/filters/http-exception.filter';
 import { PrismaService } from '../src/prisma/prisma.service';
@@ -66,9 +66,10 @@ describe('Polls App E2E Tests', () => {
       expect(response.headers['set-cookie']).toBeDefined();
 
       accessToken = response.body.accessToken;
-      const cookieHeader = response.headers['set-cookie'] as string[];
-      const refreshCookie = cookieHeader.find((c) =>
-        c.startsWith('refresh_token='),
+      const cookieHeader = response.headers['set-cookie'];
+      const cookieArray = Array.isArray(cookieHeader) ? cookieHeader : [cookieHeader];
+      const refreshCookie = cookieArray.find((c) =>
+        c && c.startsWith('refresh_token='),
       );
       refreshToken = refreshCookie?.split(';')[0].replace('refresh_token=', '') || '';
     });
